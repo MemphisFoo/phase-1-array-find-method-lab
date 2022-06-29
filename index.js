79e7bda1e1cf7fc166c5ed7a3bde62f730208665
Christian Trublet de Nermont <cjwtdn@gmail.com>
2022-06-29 11:28:04 -0600

    commit tree dbd8eeb8ac797bad7229c84f2e913d9e4184e59d

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+//code your solution here
+superbowlWin = (record) =>{
+    const result = record.find(record => record.result === "W")
+    return !! result? result.year: undefined;
+}
+
+
+/*function superbowlWin(record){
+    for(let item of record)
+    if(record.result === "W"){
+        return (record.year)}
+    else if(record.result !== "W"){
+    return 'undefined'}};
+    */
